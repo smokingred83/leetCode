39f600a84ffcbae53bb1d45f088038ef24f7ee55
Giancarlo <smokingred83@yahoo.it>
2026-01-26 17:19:32 +0100

    Adding a solution to the "mySqrt" leetcode problem

--- mySqrt.py
+++ mySqrt.py
@@ -1,6 +1,30 @@
 def mySqrt(x: int) -> int:
-    pass
+    if x <= 1:
+        return x
+    res = left = 1
+    right = x // 2
+    while left <= right:
+        middle = (right + left) // 2
+        power = middle ** 2
+        if power == x:
+            return middle
+        if power < x:
+            res = left
+            left = middle + 1
+        else:
+            res = right = middle -1
+    return res
+
+def mySqrt1(x: int) -> int:
+    if x <= 1:
+        return x
+    res = x // 2
+    while res ** 2 > x:
+        res //= 2
+    while x // res >= res:
+        res += 1
+    return res - 1
 
 if __name__ == "__main__":
-    res = mySqrt(8)
+    res = mySqrt(15)
     res
