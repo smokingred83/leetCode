e6a01cb96301cfe600894f2cde9865296860d4ff
Giancarlo <smokingred83@yahoo.it>
2026-02-07 18:17:33 +0100

    Adding a dvide-and-conquer solution to "maxSubArray" leetcode problem

--- leetcode/maxSubArray.py
+++ leetcode/maxSubArray.py
@@ -5,6 +5,14 @@ def maxSubArray(nums: [int]) -> int:
         _max = max(_max, current)
     return _max
 
+def maxSubArray_divideConquer(nums: [int]) -> int:
+    def max_sub_array(nums: [int], _max: int, current: int, idx: int):
+        if idx >= len(nums):
+            return _max
+        tmp = max(current + nums[idx], nums[idx])
+        return max_sub_array(nums, max(_max, tmp), tmp, idx + 1)
+    return max_sub_array(nums, nums[0], nums[0], 1)
+
 def maxSubArray_old(nums: [int]) -> int:
     current = result = nums[0]
     for i in range(1, len(nums)):
@@ -17,12 +25,12 @@ def maxSubArray_old(nums: [int]) -> int:
             result = current
     return result
 
-def maxSubArray_divideConquer(nums: [int]) -> int:
+def maxSubArray_divideConquer_old(nums: [int]) -> int:
     current = result = nums[0]
     for i in range(1, len(nums)):
         current += nums[i]
         if nums[i] > current:
-            return max(result, maxSubArray_divideConquer(nums[i:]))
+            return max(result, maxSubArray_divideConquer_old(nums[i:]))
         if current > result:
             result = current
     return result
@@ -30,7 +38,7 @@ def maxSubArray_divideConquer(nums: [int]) -> int:
 if __name__ == '__main__':
     input = [-2,1,-3,4,-1,2,1,-5,4]
     res1 = maxSubArray_old(input)
-    res2 = maxSubArray_divideConquer(input)
+    res2 = maxSubArray_divideConquer_old(input)
     res1 == res2
     assert maxSubArray([1, -2, 0]) == 1
     assert maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
