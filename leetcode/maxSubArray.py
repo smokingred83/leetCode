f200c3f36d1e61f617cc82d3c43010cc06a9140b
ggallotoppan <giovanni.gallo@toppandigital.com.>
2026-02-07 13:01:04 +0100

    Adding solution to "maxSubArray" leetcode problem

--- leetcode/maxSubArray.py
+++ leetcode/maxSubArray.py
@@ -1,4 +1,11 @@
 def maxSubArray(nums: [int]) -> int:
+    _max = current = nums[0]
+    for i in range(1, len(nums)):
+        current = max(current + nums[i], nums[i])
+        _max = max(_max, current)
+    return _max
+
+def maxSubArray_old(nums: [int]) -> int:
     current = result = nums[0]
     for i in range(1, len(nums)):
         temp = current + nums[i]
@@ -22,6 +29,13 @@ def maxSubArray_divideConquer(nums: [int]) -> int:
 
 if __name__ == '__main__':
     input = [-2,1,-3,4,-1,2,1,-5,4]
-    res1 = maxSubArray(input)
+    res1 = maxSubArray_old(input)
     res2 = maxSubArray_divideConquer(input)
     res1 == res2
+    assert maxSubArray([1, -2, 0]) == 1
+    assert maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
+    assert maxSubArray([1]) == 1
+    assert maxSubArray([5, 4, -1, 7, 8]) == 23
+    assert maxSubArray([1, 2]) == 3
+    assert maxSubArray([-1, -2]) == -1
+    assert maxSubArray([8, -19, 5, -4, 20]) == 21
