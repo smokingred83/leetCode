517b960ac343aa0b0a8b20c22fb5a77213c0efc0
Giancarlo <smokingred83@yahoo.it>
2026-02-11 15:31:21 +0100

    Adding a solutin to "next larger element" GTCI problem

---
+++ grokkingTheCodingInterview/nextLargerElement.py
@@ -0,0 +1,17 @@
+def next_larger_element(arr):
+    res = [-1] * len(arr)
+    stack = []
+    for i in range(len(arr)):
+        j = i -1
+        while stack and arr[i] > stack[-1]:
+            if arr[j] == stack[-1]:
+                stack.pop()
+            if res[j] == -1:
+                res[j] = arr[i]
+            j -= 1
+        stack.append(arr[i])
+    return res
+
+if __name__ == "__main__":
+    res = next_larger_element([3, 2, 5, 1, 4, 6])
+    res
