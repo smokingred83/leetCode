654968f5af2db317879a848143c7f9c2f6338db6
ggallotoppan <giovanni.gallo@toppandigital.com.>
2026-02-11 17:14:54 +0100

    Adding alternative solution to "next larger element" gtci problem

--- grokkingTheCodingInterview/nextLargerElement.py
+++ grokkingTheCodingInterview/nextLargerElement.py
@@ -1,6 +1,18 @@
 def next_larger_element(arr):
     res = [-1] * len(arr)
     stack = []
+    for i in range(len(arr) -1, -1, -1):
+        while stack and stack[-1] <= arr[i]:
+            stack.pop()
+        if stack:
+            res[i] = stack[-1]
+        stack.append(arr[i])
+    return res
+
+
+def next_larger_element2(arr):
+    res = [-1] * len(arr)
+    stack = []
     for i in range(len(arr)):
         j = i -1
         while stack and arr[i] > stack[-1]:
