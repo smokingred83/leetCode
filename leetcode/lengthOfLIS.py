bfd7f35fc79d4347b911835d0310ae9ea8f13b25
Giancarlo <smokingred83@yahoo.it>
2026-03-16 17:52:40 +0100

    Adding a O(nlogn) solution to lengthOfLIS leetcode problems

--- leetcode/lengthOfLIS.py
+++ leetcode/lengthOfLIS.py
@@ -1,6 +1,28 @@
 def lengthOfLIS(nums: [int]) -> int:
     result = []
     for i in range(len(nums)):
+        pos = search(result, nums[i])
+        if pos >= len(result):
+            result.append(nums[i])
+        else:
+            result[pos] = min(result[pos], nums[i])
+    return len(result)
+
+def search(nums: [int], tgt: int) -> int:
+    left, right = 0, len(nums) -1
+    while left <= right:
+        middle = (left + right) // 2
+        if nums[middle] == tgt:
+            return middle
+        if nums[middle] < tgt:
+            left = middle + 1
+        elif nums[middle] > tgt:
+            right = middle -1
+    return left
+
+def lengthOfLIS_cuadratic(nums: [int]) -> int:
+    result = []
+    for i in range(len(nums)):
         prev = 0
         for j in range(i - 1, -1, -1):
             if nums[j] < nums[i]:
