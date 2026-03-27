7fe510168f7c75737f68fa18a6cb4ea8d1e1f7d4
ggallotoppan <giovanni.gallo@toppandigital.com.>
2026-03-27 18:30:07 +0100

    Adding a recursive solution to "connect" leetcode problems

--- leetcode/connect.py
+++ leetcode/connect.py
@@ -14,6 +14,22 @@ class Node:
 def connect(root: Optional[Node]) -> Optional[Node]:
     if root is None:
         return None
+    if root.left is not Node:
+        connect_recur(root.left, root.right)
+    return root
+
+def connect_recur(left: Optional[Node], right: Optional[Node]):
+    if left is None:
+        return
+    connect_recur(left.left, left.right)
+    connect_recur(left.right, right.left)
+    connect_recur(right.left, right.right)
+    left.next = right
+
+
+def connect1(root: Optional[Node]) -> Optional[Node]:
+    if root is None:
+        return None
     q = Deque([root])
     while q:
         current = None
@@ -29,12 +45,18 @@ def connect(root: Optional[Node]) -> Optional[Node]:
     return root
 
 if __name__ == "__main__":
+    n15 = Node(val=15)
+    n14 = Node(val=14)
+    n13 = Node(val=13)
+    n12 = Node(val=12)
+    n11 = Node(val=11)
+    n10 = Node(val=10)
     n9 = Node(val=9)
-    n8 = Node(val=8, left=n9)
-    n7 = Node(val=7)
-    n6 = Node(val=6)
-    n5 = Node(val=5)
-    n4 = Node(val=4)
+    n8 = Node(val=8)
+    n7 = Node(val=7, left=n14, right=n15)
+    n6 = Node(val=6, left=n12, right=n13)
+    n5 = Node(val=5, left=n10, right=n11)
+    n4 = Node(val=4, left=n8, right=n9)
     n3 = Node(val=3, left=n6, right=n7)
     n2 = Node(val=2, left=n4, right=n5)
     n1 = Node(val=1, left=n2, right=n3)
