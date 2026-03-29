c1c2f52f4fe16c633de6a2964583a1e2775da45e
Giancarlo <smokingred83@yahoo.it>
2026-03-29 21:07:14 +0200

    Adding another (non-complete) solution to longestPalindrome leetcode problem

--- leetcode/longestPalindrome.py
+++ leetcode/longestPalindrome.py
@@ -1,4 +1,20 @@
 def longestPalindrome(s: str) -> str:
+    if len(s) == 1:
+        return s[0]
+    longest_l, longest_r, left = 0, 0, 0
+    for right in range(len(s)):
+        if left > 0 and s[left -1] == s[right]:
+            left -= 1
+        elif s[max(0, right - 2)] == s[right]:
+            left = left if s[left] == s[right] else max(0, right - 2)
+        else:
+            left = right -1 if s[right - 1] == s[right] else right
+        if (right - left) > (longest_r - longest_l):
+            longest_l = left
+            longest_r = right
+    return s[longest_l:longest_r+1]
+
+def longestPalindrome1(s: str) -> str:
     result = s[0]
     for i in range(len(s)):
         left, right, delta = 0, i, 0
@@ -15,5 +31,5 @@ def longestPalindrome(s: str) -> str:
     return result
 
 if __name__ == "__main__":
-    res = longestPalindrome("aaaabaaa") # abbcccba
+    res = longestPalindrome("bacabab") # abb bananas aacabdkacaa abbcccba aaaabaaa cbbd
     res
